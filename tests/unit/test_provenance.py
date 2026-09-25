7dd909360440263a8f3b44ecf98abf0b59062783
Pedro Vinícius Patrício Silva <pedroviniciusodt@gmail.com>
2026-09-25 20:28:12 -0300

    chore: finish lint cleanup in tests/unit/test_provenance.py

--- tests/unit/test_provenance.py
+++ tests/unit/test_provenance.py
@@ -3,8 +3,8 @@ from types import SimpleNamespace
 
 from fastapi.testclient import TestClient
 
-from src.api.main import app
 import src.api.routers.provenance as provenance_router
+from src.api.main import app
 
 client = TestClient(app)
 
